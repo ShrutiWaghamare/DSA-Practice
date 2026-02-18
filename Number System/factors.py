5660beb2c767b6d00fd3c7109a78d9f72dc483f9
Shruti <shrutiw796@gmail.com>
2026-02-18 21:05:59 +0530

    factors

---
+++ Number System/factors.py
@@ -0,0 +1,9 @@
+def factors(n):
+    result = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            result.append(i)
+    return result
+    
+n = 3
+print(f"Factors of {n} is: ", factors(n))
