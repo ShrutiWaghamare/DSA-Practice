1ba7384eab5be9c110449d14db073c0da77c5dbc
Shruti <shrutiw796@gmail.com>
2026-02-28 22:27:56 +0530

    In place

---
+++ Array/Remove in place.py
@@ -0,0 +1,10 @@
+def removeDuplicates_sorted(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
