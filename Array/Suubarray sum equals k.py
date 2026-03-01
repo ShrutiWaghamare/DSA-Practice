ee411e6a158932f7a5a4f0efcc53ddb12bf360b6
Shruti <shrutiw796@gmail.com>
2026-03-01 23:55:46 +0530

    Arr

---
+++ Array/Suubarray sum equals k.py
@@ -0,0 +1,11 @@
+def subarraySum(nums, k):
+    count = 0
+    
+    for i in range(len(nums)):
+        total = 0
+        for j in range(i, len(nums)):
+            total += nums[j]
+            if total == k:
+                count += 1
+    
+    return count
