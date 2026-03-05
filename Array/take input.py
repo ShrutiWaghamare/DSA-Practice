952696109e470088de6f45d866cc41fb047834da
Shruti <shrutiw796@gmail.com>
2026-03-05 21:16:11 +0530

    pyq

---
+++ Array/take input.py
@@ -0,0 +1,11 @@
+n = int(input())
+# arr = []
+# for i in range(n):
+#     num = int(input())
+#     arr.append(num)
+# print(arr)
+# arr1 = list(map(int, input().split()))
+# print(arr1)
+
+arr2 = [int(input()) for i in range(n)]
+print(arr2)
